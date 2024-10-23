75428a04dec057bc0f29f76ec048419a75ece539
Peiqi Hu <hupeiqichina@gmail.com>
2024-10-23 20:54:40 +0800

    finish exercises

--- Pandas/Data_types_And_Missing_values.py
+++ Pandas/Data_types_And_Missing_values.py
@@ -33,3 +33,27 @@ reviews.taster_twitter_handle.replace("@kerinokeefe", "@kerino")
 
 
 #### Exercises ####
+import pandas as pd
+
+reviews = pd.read_csv("../input/wine-reviews/winemag-data-130k-v2.csv", index_col=0)
+
+from learntools.core import binder; binder.bind(globals())
+from learntools.pandas.data_types_and_missing_data import *
+print("Setup complete.")
+
+#Q1 What is the data type of the points column in the dataset?
+dtype = reviews.points.dtype
+
+#Q2 Create a Series from entries in the points column, but convert the entries to strings
+point_strings = reviews.points.astype('str')
+
+#Q3 How many reviews in the dataset are missing a price?
+missing_price_reviews = reviews[reviews.price.isnull()]
+n_missing_prices = len(missing_price_reviews)
+# or n_missing_prices = reviews.price.isnull().sum()
+# or equivalently:
+# n_missing_prices = pd.isnull(reviews.price).sum()
+
+#Q4  Create a Series counting the number of times each value occurs in the region_1 field. 
+# This field is often missing data, so replace missing values with Unknown. Sort in descending order. 
+reviews_per_region = reviews.region_1.fillna('Unknown').value_counts().sort_values(ascending=False)
