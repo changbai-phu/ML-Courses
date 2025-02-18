8d48e46b8671f27d31a8d2441dbe8cb596661755
changbai-phu <changbai_817@outlook.com>
2025-02-18 21:30:05 +0800

    adding summary to Titanic tutorial code

--- Titanic_ML_from_Disaster/Tutorial.py
+++ Titanic_ML_from_Disaster/Tutorial.py
@@ -1,6 +1,31 @@
 '''
 Codes below are copied from Alexis Cook's Titanic Tutorial
 https://www.kaggle.com/code/alexisbcook/titanic-tutorial
+
+Summary of keypoints: 
+Dataset Overview: The tutorial uses the Titanic dataset, 
+    which contains information about passengers 
+    such as their age, sex, class, and whether they survived or not. 
+    The goal is to predict survival using this data.
+
+Data Exploration: It explains how to explore the data, 
+    check for missing values, and understand the relationships 
+    between different features and the target (survival).
+
+Data Preprocessing: The tutorial walks through cleaning the data 
+    by filling missing values (like age) and converting categorical data 
+    (like gender) into numerical values so that the machine learning model can use them.
+
+Modeling: The tutorial then builds a simple machine learning model 
+    using Logistic Regression. It explains how to split the data into training 
+    and testing sets, train the model, and evaluate its accuracy.
+
+Submission: Lastly, it shows how to make predictions on the test set 
+    and submit the results to Kaggle’s Titanic competition.
+
+Overall, the tutorial provides a step-by-step guide on how to work with a dataset, 
+clean and preprocess the data, build a model, and make predictions, 
+all while introducing basic machine learning concepts.
 '''
 
 import numpy as np # linear algebra
@@ -18,10 +43,16 @@ test_data.head()
 women = train_data.loc[train_data.Sex == 'female']["Survived"]
 rate_women = sum(women)/len(women)
 print("% of women who survived:", rate_women)
+'''
+% of women who survived: 0.7420382165605095
+'''
 
 men = train_data.loc[train_data.Sex == 'male']["Survived"]
 rate_men = sum(men)/len(men)
 print("% of men who survived:", rate_men)
+'''
+% of men who survived: 0.18890814558058924
+'''
 
 from sklearn.ensemble import RandomForestClassifier
 
